1af50a0dc91dae8bd50f6a1d13ec602a5d9ed180
Michael Gratton <michaelg@getup.org.au>
2020-10-26 14:28:32 +1100

    Fix test ordering

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,11 +6,11 @@ describe('allLoopsBack', () => {
   const complexGraph = [ [1, 2], [2, 3], [1, 3], [3, 4], [4, 2], [5, 6], [2, 1] ];
 
   it('should return an empty list if no looping paths exist', () => {
-    expect(allLoopsBack(2, simpleGraph)).to.eql([[ 2, 3, 4, 2 ]]);
+    expect(allLoopsBack(1, simpleGraph)).to.eql([]);
   });
 
   it('should return a list off all existing looping paths', () => {
-    expect(allLoopsBack(1, simpleGraph)).to.eql([]);
+    expect(allLoopsBack(2, simpleGraph)).to.eql([[ 2, 3, 4, 2 ]]);
   });
 
   it('should return multiple paths sorted form shortest to longest', () => {
